63ff42ef5f5cc13dea80c3edad2d729e1260627c
Triss Healy <th@trissyle.gs>
2023-01-24 13:25:43 +1100

    Update for apple silicon and gcc 12

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,14 +1,24 @@
 require 'formula'
 
 class ArmNoneEabiGcc < Formula
+  desc "GNU compiler collection for ARM embedded targets"
+  homepage 'https://developer.arm.com/Tools%20and%20Software/GNU%20Toolchain'
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+  version '12.2.rel1'
+  license 
 
-  homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
-  version '10.3-2021.07'
+  on_macos do
+    on_intel do 
+      url 'https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/arm-gnu-toolchain-12.2.rel1-darwin-x86_64-arm-none-eabi.tar.xz'
+    end
 
-  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.07/gcc-arm-none-eabi-10.3-2021.07-mac-10.14.6.tar.bz2'
-  sha256 '0a4554b248a1626496eeba56ad59d2bba4279cb485099f820bb887fe6a8b7ee4'
+    on_arm do 
+      url 'https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/arm-gnu-toolchain-12.2.rel1-darwin-arm64-arm-none-eabi.tar.xz'  
+      sha256 '21a9e875250bcb0db8df4cb23dd43c94c00a1d3b98ecba9cdd6ed51586b12248'
+    end
+  end
 
-  def install
+  def install-
     (prefix/"gcc").install Dir["./*"]
     Dir.glob(prefix/"gcc/bin/*") { |file| bin.install_symlink file }
   end
